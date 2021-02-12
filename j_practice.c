42eed83fdbc3f97539dbbe87d7c2387eef987e9f
Jinto2021 <78880395+Jinto2021@users.noreply.github.com>
2021-02-12 18:16:49 -0500

    Update j_practice.c

--- j_practice.c
+++ j_practice.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+
+float addition (float a, float b)
+{
+  float c = a + b;
+  printf("%f + %f = %f \n",a, b, c);
+  return c;
+}
+
+
 void j_practice()
 {
   printf("Welcome to functions by Jinto\n Best wishes!\n" );
