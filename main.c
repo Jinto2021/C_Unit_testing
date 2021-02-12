3ac645ac5cb123828fcc6c8c387d3317c74672fc
Jinto2021 <78880395+Jinto2021@users.noreply.github.com>
2021-02-12 17:42:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include "main.h"
 
 int main(){
-  printf("Hello World");
+  printf("Hello World\n");
 //  a_practice();
   j_practice();
 //  p_practice();
 //  s_practice();
 //  v_practice();
-}
+  
+  return 0;
+ }
