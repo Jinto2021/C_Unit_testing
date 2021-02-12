bce07e611575e933dd796a1a50b203ff1187bc25
Jinto2021 <78880395+Jinto2021@users.noreply.github.com>
2021-02-12 15:52:07 -0500

    Update j_practice.c
    added addition and subtraction functions

--- j_practice.c
+++ j_practice.c
@@ -1,1 +1,15 @@
 
+float addition (float a, float b)
+{
+  float c = a + b;
+  printf("%f + %f = %f \n",a, b, c);
+  return c;
+}
+
+
+float subtraction (float a, float b)
+{
+  float c = a - b;
+  printf("%f - %f = %f \n",a, b, c);
+  return c;
+}
